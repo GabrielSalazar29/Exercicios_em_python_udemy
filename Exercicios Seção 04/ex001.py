a99aa9da137b455383c7420b5c37e08fc8f44c3e
Gabriel Salazar Araujo Alcantara <gsaalcantara@gmail.com>
2021-04-30 04:14:44 -0300

    udemy

--- Exercicios Seção 04/ex001.py
+++ Exercicios Seção 04/ex001.py
@@ -2,4 +2,4 @@
 Faça um programa que leia um numero inteiro e o imprima.
 """
 x = int(input("Escreva um numero inteiro: "))
-print(f"O numero digitado foi = {x}")
+print(f"O numero digitado foi: {x}")
---
+++ Exercicios Seção 04/ex002.py
@@ -0,0 +1,5 @@
+"""
+Faça um programa que leia um numero real e o imprima.
+"""
+x = float(input("Digite um numero real: "))
+print(f"O numero digitado foi: {x}")
