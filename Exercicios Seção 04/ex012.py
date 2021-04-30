373c8686953e5173a466c3f743f755ad0c9ed4ac
Gabriel Salazar Araujo Alcantara <gsaalcantara@gmail.com>
2021-04-30 15:07:40 -0300

    udemy

--- Exercicios Seção 04/ex012.py
+++ Exercicios Seção 04/ex012.py
@@ -6,4 +6,4 @@ conversão é: K = 1.61 * M, sendo K a distância em quilômetros e M em milhas.
 
 dm = float(input("Digite uma distância em milhas: "))
 dk = 1.61 * dm
-print(f"Essa distância convertida em km fica: {dk}km")
+print(f"Essa distância convertida em km fica: {dk:.2f}km")
