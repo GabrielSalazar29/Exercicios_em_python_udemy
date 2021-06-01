499db6cc3750a46abf1e5d4d77a491e76d1e0070
Gabriel Salazar Araujo Alcantara <gsaalcantara@gmail.com>
2021-05-31 23:06:41 -0300

    udemy

--- Exercicios Seção 07 parte 1/ex016.py
+++ Exercicios Seção 07 parte 1/ex016.py
@@ -6,3 +6,24 @@ escreva uma mensagem informando que o código é inválido.
 """
 
 
+vetor = []
+for c in range(5):
+    vetor.append(float(input(f"Digite o numero da posição {c}: ")))
+while True:
+    print("="*20)
+    op = int(input("Digite 0 para sair.\n"
+               "Digite 1 para mostrar o vetor na ordem direta.\n"
+               "Digite 2 para mostrar o vetor na ordem inversa.\n"
+               "OPÇÃO: "))
+    print("=" * 20)
+    if op == 0:
+        print("Encerrando...")
+        break
+    elif op == 1:
+        vetor.sort()
+        print(vetor)
+    elif op == 2:
+        vetor.reverse()
+        print(vetor)
+    else:
+        print("O código é inválido")
