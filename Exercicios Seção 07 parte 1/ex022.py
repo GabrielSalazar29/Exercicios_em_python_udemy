836084e1fe635fbbfd9a7339d3bfcb3a1873f42a
Gabriel Salazar Araujo Alcantara <gsaalcantara@gmail.com>
2021-06-10 01:34:53 -0300

    udemy

--- Exercicios Seção 07 parte 1/ex022.py
+++ Exercicios Seção 07 parte 1/ex022.py
@@ -1,3 +1,19 @@
 """
-
+Faça um programa que leia dois vetores de 10 posições e calcule outro vetor contendo,
+nas posições pares os valores do primeiro e nas posições impares os valores do se-
+gundo.
 """
+
+
+A = []
+B = []
+C = []
+for i in range(10):
+    A.append(int(input(f"Digite um numero para o Primeiro vetor na posição {i}: ")))
+    B.append(int(input(f"Digite um numero para o Segundo vetor na posição {i}: ")))
+    if i % 2 == 0:
+        C.append(A[i])
+    else:
+        C.append(B[i])
+
+print(C)
